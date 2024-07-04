70481d96c209e8689be489f9eb12eed73d81ffb4
結城康太 <kota.2199@icloud.com>
2024-07-05 04:12:21 +0900

    add title ui(saikou)

--- Assets/Geek/Scripts/TitleController.cs
+++ Assets/Geek/Scripts/TitleController.cs
@@ -8,22 +8,90 @@ public class TitleController : MonoBehaviour
     [SerializeField]
     private FadeInOut fadeController;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject explainPanel;
+
+    private bool onSelect = false;
+
+    public string teamAlphabet;
+
+    [SerializeField]
+    private Text teamAlphabetText;
+
+    [SerializeField]
+    private string[] teamAlphabets;
+
+    private int teamNumber = 1;
+
+    private void Start()
     {
-        
+        teamAlphabet = "A";
+    }
+
+    private void Update()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        teamAlphabetText.text = teamAlphabet;
+    }
+
+    public void SelectOnOff()
+    {
+        if (onSelect)
+        {
+            onSelect = false;
+        }
+        else
+        {
+            onSelect = true;
+        }
+    }
+
+    public void TeamSelect(string team)
+    {
+        onSelect = false;
+        teamAlphabet = team;
+    }
+
+    public void PlusTeamAlphabet()
+    {
+        teamNumber++;
+        if(teamNumber > teamAlphabets.Length)
+        {
+            teamNumber = 1;
+        }
+        teamAlphabet = teamAlphabets[teamNumber -1];
+    }
+
+    public void MinusTeamAlphabet()
+    {
+        teamNumber--;
+        if (teamNumber < 1)
+        {
+            teamNumber = teamAlphabets.Length;
+        }
+        teamAlphabet = teamAlphabets[teamNumber -1];
     }
 
     // Update is called once per frame
     public void ToNextScene(string sceneName)
     {
-        StartCoroutine(ToGame(sceneName));
+        StartCoroutine(ToGame());
     }
 
-    private IEnumerator ToGame(string sceneName)
+    private IEnumerator ToGame()
     {
         IEnumerator enumerator = fadeController.FadeOut();
         yield return enumerator;
         SceneController.instance.ToGame();
+    }
+
+    public void GameStart()
+    {
+        explainPanel.SetActive(true);
+        SendWebRequest.instance.team = teamAlphabet;
     }
 }
